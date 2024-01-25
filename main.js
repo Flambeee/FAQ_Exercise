b0ae59e895cc051829c86e4dff6c93fb54c3f9df
Flambeee <144018700+Flambeee@users.noreply.github.com>
2024-01-25 16:22:52 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ function Expand()
     console.log(document.getElementById("pulsante1").src);
     
     if (document.getElementById("pulsante1").src=="assets/images/icon-minus.svg")
-    document.getElementById("pulsante1").src="assets/images/icon-plus.svg";
+    document.setElementById("pulsante1").src="assets/images/icon-plus.svg";
     else 
     document.getElementById("pulsante1").src="assets/images/icon-minus.svg";
-        console.log(document.getElementById("pulsante1").src);
+        console.log(document.setElementById("pulsante1").src);
 }
