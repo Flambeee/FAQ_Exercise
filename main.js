79883ae2831065d30fad9602ec6e1c6cc9fd980d
Flambeee <144018700+Flambeee@users.noreply.github.com>
2024-01-25 22:25:10 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ var i=1;
 
 function Expand()
 {
-    if (i % 2 == 0)
+    if (i % 2 == 0){
+
         document.getElementById("pulsante1").src="assets/images/icon-plus.svg";
-    else
+        document.getElementById("risposta1").innerText= " ";
+    }
+
+    else{
+
         document.getElementById("pulsante1").src="assets/images/icon-minus.svg";
+        document.getElementById("risposta1").innerText="Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.";
+    }
     i++;
 }
