0d863951ca2e36e83d483ca4fe04344dcae3668e
Flambeee <144018700+Flambeee@users.noreply.github.com>
2024-01-25 21:23:49 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
+var i=1;
+
 function Expand()
 {
-    console.log(document.getElementById("pulsante1").src);
-    
-    if (document.getElementById("pulsante1").src==="assets/images/icon-minus.svg")
-    document.getElementById("pulsante1").src="assets/images/icon-plus.svg";
-    else 
-    document.getElementById("pulsante1").src="assets/images/icon-minus.svg";
-        console.log(document.getElementById("pulsante1").src);
+    if (i % 2 == 0)
+        document.getElementById("pulsante1").src="assets/images/icon-plus.svg";
+    else
+        document.getElementById("pulsante1").src="assets/images/icon-minus.svg";
+    i++;
 }
